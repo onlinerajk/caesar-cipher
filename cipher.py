b88190d430fce63ad900629d94ca94e092f89c07
Raj K <onlinerajk@gmail.com>
2024-11-14 13:37:58 -0500

    Project Complete to run

--- cipher.py
+++ cipher.py
@@ -1,1 +1,21 @@
 # add your code here
+def caesar_cipher(text, shift=5):
+    encrypted_text = ""
+    
+    for char in text:
+        if char.isupper():
+            encrypted_text += chr((ord(char) - 65 + shift) % 26 + 65)
+        elif char.islower():
+            encrypted_text += chr((ord(char) - 97 + shift) % 26 + 97)
+        else:
+            encrypted_text += char
+            
+    return encrypted_text
+
+# Ask the user for input
+plain_text = input("Enter the text to encrypt: ")
+
+# Encrypt and print the result
+encrypted_text = caesar_cipher(plain_text)
+print("Encrypted text:", encrypted_text)
+
